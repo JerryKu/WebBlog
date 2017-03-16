599e6c7605c3a04f410d7fdac110726d1b40807d
Jerry Ku <jerryku1220@gmail.com>
2017-03-16 10:53:33 +0000

    finished up edit/show

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var express = require("express");
 var app = express();
+var methodOverride = require("method-override");
 
 //App Config
 mongoose.connect("mongodb://localhost/BlogApp");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(methodOverride("_method"));
 //Restful Routes
 var blogSchema = new mongoose.Schema({
     title: String,
@@ -80,8 +81,38 @@ app.get("/blogs/:id", function(req, res){
 
 //EDIT
 app.get("/blogs/:id/edit",function(req,res){
-    res.render("edit");
-})
+    Blog.findById(req.params.id,function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.render("edit", {blog:foundBlog});
+        }
+    });
+});
+
+//UPDATE
+app.put("/blogs/:id", function(req,res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+//DELETE 
+app.delete("/blogs/:id", function(req,res){
+    
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.redirect("/blogs");
+        }
+    })
+});
+    
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
